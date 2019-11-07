c4ba19faa7b47cf7ad68c508470116832f83f6e5
arjunindia <arjun8107@gmail.com>
2019-11-07 19:17:03 +0530

    added major files

---
+++ public/js/anim.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+anime({
+  targets: '.scr',
+  direction: 'alternate',
+  duration: 80
+});
+
+
+particlesJS.load('bd', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+
+var options = {
+  strings: ["Arjun","Epic Dev"],
+  typeSpeed: 10,
+  backDelay: 1000,
+  loop:true
+};
+
+var options2 = {
+  strings: ["This is my homepage. Hope you guys like it!"],
+  startDelay: 3000,
+  typeSpeed: 10,
+  backDelay: 1000,
+  
+  showCursor:false
+};
+
+var typed = new Typed('.element', options);
+var typed2 = new Typed('#shell-text', options2);
+ctx.strokeStyle = ctxBars.strokeStyle = ctxBars.fillStyle = 'rgba(69, 212, 12, 0.5)';
+
+});
