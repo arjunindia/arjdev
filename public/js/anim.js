b4cf9200dcbce5372c8fd46e94e4a19aa94499a3
arjunindia <arjun8107@gmail.com>
2019-11-07 22:05:41 +0530

    "music added"

--- public/js/anim.js
+++ public/js/anim.js
@@ -18,6 +18,10 @@ var options = {
   loop:true
 };
 
+
+
+
+
 var options2 = {
   strings: ["This is my homepage. Hope you guys like it!"],
   startDelay: 3000,
@@ -34,4 +38,28 @@ var typed = new Typed('.element', options);
 var typed2 = new Typed('#shell-text', options2);
 ctx.strokeStyle = ctxBars.strokeStyle = ctxBars.fillStyle = 'rgba(69, 212, 12, 0.5)';
 
+var pixelwave = new PixelWave({
+    // options like:
+    // canvasTop: 0
+});
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
 });
